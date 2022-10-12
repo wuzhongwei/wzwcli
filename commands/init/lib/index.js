d5c76dc08472c701a2002fbb262217913b44b4f7
wuzhongwei <94050803@qq.com>
2022-10-12 23:17:17 +0800

    添加命令

---
+++ commands/init/lib/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+function init(projectName, cmdObj) {
+  console.log('init', projectName, cmdObj.force)
+}
+module.exports = init;
--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -7,22 +7,70 @@ const colors = require('colors/safe')
 const userHome = require('user-home')
 const pathExists = require('path-exists').sync
 const log = require('@imooc-cli-dev/log')
+const init = require('@imooc-cli-dev/init')
 const pkg = require('../package.json')
 const constant = require('./const')
+const commander = require('commander')
 let args;
+
+const program = new commander.Command()
+const options = program.opts()
 async function core() {
   try{
     checkPkgVersion()
     checkNodeVersion()
     checkRoot()
     checkUserHome()
-    checkInputArgs()
+    // checkInputArgs()
     checkEnv()
     await checkGlobalUpdate()
+    registerCommand()
   }catch(e){
     log.error(e.message)
   }
   
+}
+
+// 注册命令
+function registerCommand() {
+  
+  program
+    .name(Object.keys(pkg.bin)[0])
+    .usage('<command> [options]')
+    .version(pkg.version)
+    .option('-d, --debug', '是否开启调试模式', false)
+
+  program
+    .command('init [projectName]')
+    .option('-f, --force', '是否强制初始化项目')
+    .action(init)
+  
+  // 开启debug模式
+  program.on('option:debug', function() {
+    if (options.debug) {
+      process.env.LOG_LEVEL = 'verbose'
+    } else {
+      process.env.LOG_LEVEL = 'info'
+    }
+    log.level = process.env.LOG_LEVEL
+  })
+
+  // 对未知命令监听
+  program.on('command:*', function(obj) {
+    const availableCommands = program.commands.map(cmd => cmd.name())
+    console.log(colors.red('未知的命令: ' + obj[0]))
+    if (availableCommands.length > 0) {
+      console.log(colors.red('可用命令: ' + availableCommands.join(',')))
+    }
+  })
+  
+  program.parse(process.argv)
+
+  if (program.args && program.args.length < 1) {
+    program.outputHelp()
+    console.log()
+  }
+
 }
 // 更新版本号
 async function checkGlobalUpdate() {
@@ -33,7 +81,6 @@ async function checkGlobalUpdate() {
   if (lastVersion && semver.gt(lastVersion, currentVersion)) {
     log.warn('更新提示', colors.yellow(`请手动更新 ${npmName}, 当前版本: ${currentVersion}, 最新版本: ${lastVersion}更新命令：npm install -g ${npmName}`))
   }
-  console.log('lastVersion', lastVersion, currentVersion)
 }
 // 环境变量检查功能开发
 function checkEnv() {
